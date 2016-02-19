4c82f45ec0a317e219f24b68723660e1fca2d3fd
mk <mihasiksli@yandex.ru>
2016-02-19 14:44:50 +0300

    controls

--- static/_games/penguin/game.js
+++ static/_games/penguin/game.js
@@ -30,21 +30,15 @@ MyGame.preloadState.prototype = {
         this.load.onFileComplete.add(this.fileComplete, this);
 
 
-        this.load.json('gameText', assetsDir + '/json/game_text.json')
+        this.load.json('gameText', assetsDir + '/json/game_text.json'),
         this.load.image('bgTitle', assetsDir + '/sprites/title_screen.png'),
         this.load.image('bgMain', assetsDir + '/sprites/background.png'),
-        this.load.image('bgMain', assetsDir + '/sprites/background.png')
-//        this.load.image('bg_title', assetsDir + '/img/bg_title.png'),
- //       this.load.image('roadkill_road', assetsDir + '/img/roadkill/road.png'),
-  //      this.load.spritesheet('roadkill_bulldozer', assetsDir + '/img/roadkill/bulldozer.png', 150, 150),
-  //      this.load.spritesheet('roadkill_food', assetsDir + '/img/roadkill/food.png', 70, 95),
-
+        this.load.spritesheet('character', assetsDir + '/sprites/character.png', 285, 380)
 
     },
 
     create: function() {
-        //console.log(text.text);
-        this.game.startNewGame(); /*this.state.start('title')*/
+        this.state.start('title');
     },
 
     fileComplete: function(progress) {
@@ -62,10 +56,13 @@ MyGame.titleScreenState.prototype = {
         this.background = this.add.sprite(0, 0, 'bgTitle');
         this.input.onDown.add(this.onDown, this);
 
+        //this.bulldozer = this.add.sprite(0, this.drivePoint - 75, 'roadkill_bulldozer');
+
+
+
     },
     onDown: function(pointer) {
         this.state.start('main');
-
     }
 },
 
@@ -78,127 +75,59 @@ MyGame.mainState.prototype = {
 
     create: function() {
         this.add.sprite(0, 0, "bgMain");
-    }
-},
 
+        this.character = this.add.sprite(0, 0, 'character');
 
-MyGame.infoState = function(t) {},
-MyGame.infoState.prototype = {
-    init: function(score, isDescription, infoText) {
-        this.localScore = score;
-        this.currentStateName = this.game.getCurrentStateName();
-        this.infoText = infoText;
-        this.isDescription = isDescription;
-        this.currentScore = 0;
-        this.scorePart = this.localScore / 10;
-        this.world.setBounds(0, 0, this.game.width, this.game.height);
+        this.character.x = this.game.width / 2;
+        this.character.y = this.game.height - this.character.height;
+        this.character.anchor.x = 0.5;
+        this.character.handPosition = -1;
 
-    },
+        this.character.animations.add('idle', [3], 0, true);
+        this.character.animations.add('angry', [0], 0, true);
 
-    create: function() {
-        this.showBg();
-        this.infoWindow = this.add.sprite( this.game.width / 2, -this.game.height/2, 'infoWindow');
-        this.infoWindow.inputEnabled = true;
-        if (this.isDescription) {
-            this.infoWindow.events.onInputDown.add(function() {
-                this.state.start(this.currentStateName);
-            }, this);
-        }
-        this.physics.arcade.enable(this.infoWindow);
-        this.infoWindow.anchor.set(0.5, 0.5);
-        this.infoWindow.body.velocity.y = 1800;
+        this.character.animations.add('low', [2], 0, true);
+        this.character.animations.add('high', [4], 0, true);
+        this.character.animations.add('catch', [1], 0, true);
 
-        this.scoreText = this.add.text(this.game.width / 2, this.game.height / 2, '423', {font: '40px Arial', fill: '#ffffff'})
-        this.scoreText.visible = false;
-        this.scoreText.anchor.set(0.5, 0.5);
+        this.character.animations.play('idle');
 
-        this.buttonReplay = this.add.sprite(95, this.game.height - 290, 'buttons');
-        this.buttonReplay.inputEnabled = true;
-        this.buttonReplay.events.onInputDown.add(function() {
-            this.game.startNextLevel(true);
-        }, this);
-        this.buttonReplay.scale.set(0.7, 0.7);
+        this.input.onDown.add(this.onDown, this);
 
-        this.buttonNext = this.add.sprite(this.game.width - 305, this.game.height - 290, 'buttons');
-        this.buttonNext.animations.add('next', [1]); this.buttonNext.animations.play('next');
-        this.buttonNext.inputEnabled = true;
-        this.buttonNext.events.onInputDown.add(function() {
-            this.game.globalScore += this.localScore;
-            this.game.startNextLevel(false);
-        }, this);
-        this.buttonNext.scale.set(0.7, 0.7);
-
-        this.buttonNext.visible = false;
-        this.buttonReplay.visible = false;
-
-        this.timer = this.time.create(false);
-
-        this.timer.loop(100, this.showInfo, this);
-
+        this.penguins = this.add.group();
+        this.penguins.enableBody = true;
     },
 
     update: function() {
-        if (this.infoWindow.body.velocity.y != 0 && this.infoWindow.y > this.game.height/2) {
-            this.infoWindow.body.velocity.y = 0;
-            this.infoWindow.y = this.game.height * 0.5;
-            if (!this.isDescription) this.scoreText.visible = true;
-            this.timer.start();
+        if (this.character.handPosition == 0 || this.character.handPosition == 1) {
+            this.character.scale.x = -1;
         }
-        if (!this.isDescription) this.scoreText.text = 'Ваш счет: ' + (this.currentScore <= this.localScore ?   Math.floor(this.currentScore) : this.localScore);
-    },
-    showInfo: function() {
-        if (!this.isDescription) {
-            this.currentScore += this.scorePart;
+        else this.character.scale.x = 1;
 
-            if (this.currentScore >= this.localScore) {
-                this.currentScore = this.localScore;
-                this.timer.stop();
-                this.buttonNext.visible = true;
-                this.buttonReplay.visible = true;
-            }
-        }
-        else {
-            this.showDescription();
-            this.timer.stop();
-        }
-    },
-    showDescription: function() {
-        //console.log('Описание');
     },
 
-    showBg: function() {
-        if (this.currentStateName == 'gameShootingGallery') {
-            this.add.sprite(0, 0, 'shootingGallery_bg');
-            this.panelUp = this.add.sprite(0, 200, 'shootingGallery_panels');
-            this.panelUp.animations.add('up', [0]);
-            this.panelUp.animations.play('up');
-            this.panelMiddle = this.add.sprite(0, 350, 'shootingGallery_panels');
-            this.panelMiddle.animations.add('middle', [1]);
-            this.panelMiddle.animations.play('middle');
-            this.panelDown = this.add.sprite(0, 500, 'shootingGallery_panels');
-            this.panelDown.animations.add('down', [2]);
-            this.panelDown.animations.play('down');
+    onDown: function(pointer) {
+        if (pointer.worldX > this.game.width / 2 && pointer.worldY < this.game.height / 2) {
+            this.character.handPosition = 0;
+            this.character.animations.play('high');
         }
-        if (this.currentStateName == 'gameKissing') this.add.sprite(0, 0, 'kissing_bg');
-
-        if (this.currentStateName == 'gamePancakes') this.add.sprite(0, 0, 'pancakes_bg');
-        if (this.currentStateName == 'gameRoadkill') {
-            for (var i = 0; i < this.game.width / 60 + 1; i++ ) {
-                this.add.image(i * 60, 0, 'roadkill_road');
-            }
+        else if (pointer.worldX > this.game.width / 2 && pointer.worldY >= this.game.height / 2) {
+            this.character.handPosition = 1;
+            this.character.animations.play('low');
         }
+        if (pointer.worldX < this.game.width / 2 && pointer.worldY > this.game.height / 2) {
+            this.character.handPosition = 2;
+            this.character.animations.play('low');
+        }
+        else if (pointer.worldX < this.game.width / 2 && pointer.worldY < this.game.height / 2) {
+            this.character.handPosition = 3;
+            this.character.animations.play('high');
+        }
+        console.log(this.character.handPosition);
+    },
 
-        if (this.currentStateName == 'gameFirefight') this.add.image(0, 0, 'firefight_bg');
-        if (this.currentStateName == 'gameSeagull') this.add.image(0, 0, 'seagull_bg');
-        if (this.currentStateName == 'gamePrison') this.add.sprite(0, 0, 'prison_bg');
-        if (this.currentStateName == 'gameDetective') this.add.sprite(0, 0, 'detective_bg');
-        if (this.currentStateName == 'gameTrucks') this.add.sprite(0, 0, 'trucks_road');
-        if (this.currentStateName == 'gameStrike') this.add.sprite(0, 0, 'strike_bg');
-        if (this.currentStateName == 'gameShooter') this.add.sprite(0, 0, 'shooter_bg');
 
-    }
 },
-
 
 MyGame.finishState = function(t) {},
 MyGame.finishState.prototype = {
@@ -215,29 +144,6 @@ MyGame.finishState.prototype = {
 
 window.onload = function() {
     var t = new Phaser.Game(900, 600, Phaser.AUTO, 'game_container');
-    t.startNewGame = function() {
-        this.globalScore = 0;
-        this.currentLevel = -1;
-        this.state.start('title');
-    },
-
-    /*t.startNextLevel = function(isRestart) {
-        if (!isRestart) this.currentLevel++;
-        if (this.currentLevel < 0) this.currentLevel = 0;
-        if (this.currentLevel >= gamesOrder.length || gamesOrder.lenght == 0) {
-            this.state.start('finish');
-            //this.startNewGame();
-        }
-        else this.state.start('info', true, false, 0, true);
-
-            //this.state.start(gamesOrder[this.currentLevel]);
-    },*/
-    /*t.startNewState = function() {
-
-    },
-    t.getCurrentStateName = function() {
-        return gamesOrder[this.currentLevel];
-    },*/
 
     t.global = {},
         t.state.add('boot', MyGame.bootState),
